d74c510cab30f6f5b46407b511616195a2939c3b
asiripr <asiripramodaya2000@gmail.com>
2024-09-01 11:54:54 +0530

    created create student front end page

--- src/main/java/lk/asiripr/school_management/controller/StudentController.java
+++ src/main/java/lk/asiripr/school_management/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lk.asiripr.school_management.entity.Student;
 import lk.asiripr.school_management.service.StudentService;
 
 
@@ -23,6 +24,14 @@ public class StudentController {
 	public String listStudents(Model model) {
 		model.addAttribute("students", studentService.getAllStudents());
 		return "students";
+	}
+	
+	@GetMapping("/students/new")
+	public String createStudentForm(Model model) {
+		// create student object to hold data
+		Student std = new Student();
+		model.addAttribute(std);
+		return "create_student";
 	}
 
 	
