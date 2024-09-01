ec3847df77ee0c3658f7a82056bfc5a2c84c15d8
asiripr <asiripramodaya2000@gmail.com>
2024-09-01 10:20:28 +0530

    created a front end part for this backend using thymeleaf

--- src/main/java/lk/asiripr/school_management/controller/StudentController.java
+++ src/main/java/lk/asiripr/school_management/controller/StudentController.java
@@ -1,8 +1,31 @@
 package lk.asiripr.school_management.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lk.asiripr.school_management.service.StudentService;
 
 
 @Controller
-public interface StudentController {
+public class StudentController {
+	
+	private StudentService studentService;
+
+	public StudentController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
+	}
+	
+	//handler method to handle list of students and return model and view
+	
+	@GetMapping("/students")
+	public String listStudents(Model model) {
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
+	}
+
+	
+	
+	
 }
